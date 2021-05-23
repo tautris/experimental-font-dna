57ecc98d672cbb437251af05d942685f1b0ac78e
Tautris <tautrimas.g2@gmail.com>
2021-05-23 20:57:10 +0300

    Add a large gif to test load time

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+  }
+  
+
+declare module '*.ogg' {
+    const src: string;
+    export default src;
+}
