9a26afdee5a31c6c85f5125da1a231fc398793ec
Tautris <tautrimas.g2@gmail.com>
2021-05-23 20:57:10 +0300

    Add a large gif to test load time

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import geneticProcessesMP4 from "./genetic_processes.mp4";
+import geneticProcessesOGG from "./genetic_processes.ogg";
+
 const App: React.FC = () => {
-  return <div>Experimental font DNA</div>;
+  return (
+    <div>
+      <video width="75%" autoPlay={true} muted={true} loop={true}>
+        <source src={geneticProcessesMP4} type="video/mp4" />
+        <source src={geneticProcessesOGG} type="video/ogg" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 };
 
 export { App };
