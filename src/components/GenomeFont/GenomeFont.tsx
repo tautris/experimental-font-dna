8d70a0939f9693e8fb9ad813055a2e859a9ee79b
Tautris <tautrimas.g2@gmail.com>
2021-05-30 16:27:37 +0300

    Update radio button styles

--- src/components/GenomeFont/GenomeFont.tsx
+++ src/components/GenomeFont/GenomeFont.tsx
@@ -2,20 +2,26 @@ import React from "react";
 
 import styles from "./GenomeFont.module.scss";
 
-const FONT_SIZE_OPTIONS = ["24px", "48px", "65px", "105px"];
+const FONT_SIZE_OPTIONS = ["24 px", "48 px", "65 px", "105 px"];
 
 const GenomeFont: React.FC = () => {
-  const [fontSize, setFontSize] = React.useState<string>("48px");
+  const [fontSize, setFontSize] = React.useState<string>("48 px");
 
   const handleFontSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("event.target.value", event.target.value);
+
     setFontSize(event.target.value);
+  };
+
+  const textAreaStyle = {
+    fontSize: fontSize.replace(" ", ""),
   };
 
   return (
     <div className={styles.container}>
       <div className={styles["change-font-size"]}>
         {FONT_SIZE_OPTIONS.map((option) => (
-          <>
+          <React.Fragment key={option}>
             <input
               type="radio"
               id={option}
@@ -28,13 +34,13 @@ const GenomeFont: React.FC = () => {
             <label htmlFor={option} className={styles.label}>
               {option}
             </label>
-          </>
+          </React.Fragment>
         ))}
       </div>
 
       <textarea
         className={styles.textarea}
-        style={{ fontSize }}
+        style={textAreaStyle}
         placeholder="Čia galima vesti tekstą"
         spellCheck={false}
         autoCapitalize="off"
