f83bcbd36531b0e816aa75c68ee3bbaa7bb08ef4
Tautris <tautrimas.g2@gmail.com>
2021-06-01 23:03:07 +0300

    Try out new images for font anatomy

--- src/components/GenomeFont/GenomeFont.tsx
+++ src/components/GenomeFont/GenomeFont.tsx
@@ -38,6 +38,43 @@ import x from "assets/images/letter-anatomy/x.png";
 import y from "assets/images/letter-anatomy/y.png";
 import z from "assets/images/letter-anatomy/z.png";
 import ž from "assets/images/letter-anatomy/ž.png";
+
+import a1 from "assets/images/letter-anatomy/a1.png";
+import ą1 from "assets/images/letter-anatomy/ą1.png";
+import b1 from "assets/images/letter-anatomy/b1.png";
+import c1 from "assets/images/letter-anatomy/c1.png";
+// import č1 from "assets/images/letter-anatomy/č1.png";
+// import d1 from "assets/images/letter-anatomy/d1.png";
+// import e1 from "assets/images/letter-anatomy/e1.png";
+// import ę1 from "assets/images/letter-anatomy/ę1.png";
+// import ė1 from "assets/images/letter-anatomy/ė1.png";
+// import f1 from "assets/images/letter-anatomy/f1.png";
+// import g1 from "assets/images/letter-anatomy/g1.png";
+// import h1 from "assets/images/letter-anatomy/h1.png";
+// import i1 from "assets/images/letter-anatomy/i1.png";
+// import į1 from "assets/images/letter-anatomy/į1.png";
+// import j1 from "assets/images/letter-anatomy/j1.png";
+// import k1 from "assets/images/letter-anatomy/k1.png";
+// import l1 from "assets/images/letter-anatomy/l1.png";
+// import m1 from "assets/images/letter-anatomy/m1.png";
+// import n1 from "assets/images/letter-anatomy/n1.png";
+// import o1 from "assets/images/letter-anatomy/o1.png";
+// import p1 from "assets/images/letter-anatomy/p1.png";
+// import q1 from "assets/images/letter-anatomy/q1.png";
+// import r1 from "assets/images/letter-anatomy/r1.png";
+// import s1 from "assets/images/letter-anatomy/s1.png";
+// import š1 from "assets/images/letter-anatomy/š1.png";
+// import t1 from "assets/images/letter-anatomy/t1.png";
+// import u1 from "assets/images/letter-anatomy/u1.png";
+// import ū1 from "assets/images/letter-anatomy/ū1.png";
+// import ų1 from "assets/images/letter-anatomy/ų1.png";
+// import v1 from "assets/images/letter-anatomy/v1.png";
+// import w1 from "assets/images/letter-anatomy/w1.png";
+// import x1 from "assets/images/letter-anatomy/x1.png";
+// import y1 from "assets/images/letter-anatomy/y1.png";
+// import z1 from "assets/images/letter-anatomy/z1.png";
+// import ž1 from "assets/images/letter-anatomy/ž1.png";
+
 import genomasFont from "assets/fonts/Genomas.zip";
 
 const FONT_SIZE_OPTIONS = [
@@ -61,40 +98,79 @@ const GenomeFont: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles["font-size-container"]}>
-        {FONT_SIZE_OPTIONS.map(({ value, label }) => (
-          <label key={value} className={styles["radio-wrapper"]}>
-            {label}
-            <input
-              type="radio"
-              id={value}
-              name={value}
-              value={value}
-              checked={fontSize === value}
-              className={styles.input}
-              onChange={handleFontSizeChange}
-            />
-            <span className={styles.checkmark}></span>
-          </label>
-        ))}
+      <div className={styles.section}>
+        <div className={styles["font-size-container"]}>
+          {FONT_SIZE_OPTIONS.map(({ value, label }) => (
+            <label key={value} className={styles["radio-wrapper"]}>
+              {label}
+              <input
+                type="radio"
+                id={value}
+                name={value}
+                value={value}
+                checked={fontSize === value}
+                className={styles.input}
+                onChange={handleFontSizeChange}
+              />
+              <span className={styles.checkmark}></span>
+            </label>
+          ))}
+        </div>
+
+        <textarea
+          className={styles.textarea}
+          style={{ fontSize }}
+          placeholder="Paeksperimentuok"
+          spellCheck={false}
+          autoCapitalize="off"
+          autoCorrect="off"
+        />
+
+        <div className={styles.download}>
+          <a href={genomasFont} className={styles["download-link"]}>
+            <img src={downloadIcon} alt="atsisiųsti" />
+          </a>
+        </div>
+
+        <h1 className={styles["anatomy-title"]}>Raidžių anatomija</h1>
       </div>
 
-      <textarea
-        className={styles.textarea}
-        style={{ fontSize }}
-        placeholder="Paeksperimentuok"
-        spellCheck={false}
-        autoCapitalize="off"
-        autoCorrect="off"
-      />
+      <img src={a1} {...anatomyImageProps} />
+      <img src={ą1} {...anatomyImageProps} />
+      <img src={b1} {...anatomyImageProps} />
+      <img src={c1} {...anatomyImageProps} />
+      {/* <img src={č1} {...anatomyImageProps} />
+      <img src={d1} {...anatomyImageProps} />
+      <img src={e1} {...anatomyImageProps} />
+      <img src={ę1} {...anatomyImageProps} />
+      <img src={ė1} {...anatomyImageProps} />
+      <img src={f1} {...anatomyImageProps} />
+      <img src={g1} {...anatomyImageProps} />
+      <img src={h1} {...anatomyImageProps} />
+      <img src={i1} {...anatomyImageProps} />
+      <img src={į1} {...anatomyImageProps} />
+      <img src={j1} {...anatomyImageProps} />
+      <img src={k1} {...anatomyImageProps} />
+      <img src={l1} {...anatomyImageProps} />
+      <img src={m1} {...anatomyImageProps} />
+      <img src={n1} {...anatomyImageProps} />
+      <img src={o1} {...anatomyImageProps} />
+      <img src={p1} {...anatomyImageProps} />
+      <img src={q1} {...anatomyImageProps} />
+      <img src={r1} {...anatomyImageProps} />
+      <img src={s1} {...anatomyImageProps} />
+      <img src={š1} {...anatomyImageProps} />
+      <img src={t1} {...anatomyImageProps} />
+      <img src={u1} {...anatomyImageProps} />
+      <img src={ū1} {...anatomyImageProps} />
+      <img src={ų1} {...anatomyImageProps} />
+      <img src={v1} {...anatomyImageProps} />
+      <img src={w1} {...anatomyImageProps} />
+      <img src={x1} {...anatomyImageProps} />
+      <img src={y1} {...anatomyImageProps} />
+      <img src={z1} {...anatomyImageProps} />
+      <img src={ž1} {...anatomyImageProps} /> */}
 
-      <div className={styles.download}>
-        <a href={genomasFont} className={styles["download-link"]}>
-          <img src={downloadIcon} alt="atsisiųsti" />
-        </a>
-      </div>
-
-      <h1 className={styles["anatomy-title"]}>Raidžių anatomija</h1>
       <img src={a} {...anatomyImageProps} />
       <img src={ą} {...anatomyImageProps} />
       <img src={b} {...anatomyImageProps} />
