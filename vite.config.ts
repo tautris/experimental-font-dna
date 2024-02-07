e1acfc4e7efc58f06fd6c2e5e0500d6b40bbca49
tautris <tautrimas.g2@gmail.com>
2024-02-07 13:57:30 +0200

    Update vite base public path to repo appropriate

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    resolve: {
+  base: '/experimental-font-dna/',
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
