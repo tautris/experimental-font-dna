e6c3a53d50b6e368cfb79186f0c13e1e49f73513
tautris <tautrimas.g2@gmail.com>
2024-05-09 21:48:50 +0300

    Update background for all pages

--- src/components/PageBackground/PageBackground.tsx
+++ src/components/PageBackground/PageBackground.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import classNames from "classnames";
 
 import styles from "./PageBackground.module.scss";
 
 interface Props {
-  isHomePage: boolean;
   children: React.ReactNode;
 }
 
-const PageBackground: React.FC<Props> = ({ isHomePage, children }) => (
-  <div className={classNames(isHomePage ? styles.black : styles.gradient)}>
-    <div className={styles.background}>{children}</div>
-  </div>
-);
+const PageBackground: React.FC<Props> = ({ children }) => <div className={styles.background}>{children}</div>;
 
 export { PageBackground };
