d39e4157177f92fbd85e8b535776dae1531dc165
tautris <tautrimas.g2@gmail.com>
2024-05-26 20:23:22 +0300

    Use background image for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, useLocation, generatePath, Routes, Navigate } from "react-router
 
 import { Header } from "@/components/Header/Header";
 import { HomePage } from "@/components/HomePage/HomePage";
-import { PageBackground } from "@/components/PageBackground/PageBackground";
 import { PageTransition } from "@/components/PageTransition/PageTransition";
 import { About } from "@/components/About/About";
 import { Alphabet } from "@/components/Alphabet/Alphabet";
@@ -22,24 +21,22 @@ const App: React.FC = () => {
 
   return (
     <PageTransition>
-      <PageBackground>
-        <Header isHomePage={isHomePage} />
-        <div className={isHomePage ? styles["home-page-section"] : styles.section}>
-          <Routes>
-            {/*TODO: Nest properly according to new react router standard */}
-            <Route path={PATH.HOME} element={<HomePage />} />
-            <Route path={PATH.LETTER} element={<LetterExposition />}></Route>
-            <Route path={PATH.ALPHABET_WITH_LANGUAGE} element={<Alphabet />}></Route>
-            <Route
-              path={PATH.ALPHABET}
-              element={<Navigate to={generatePath(PATH.ALPHABET_WITH_LANGUAGE, { language: LanguageType.LT })} />}
-            />
-            <Route path={PATH.GENOME_FONT} element={<GenomeFont />}></Route>
-            <Route path={PATH.ABOUT} element={<About />}></Route>
-            <Route element={<Navigate to={PATH.HOME} />} />
-          </Routes>
-        </div>
-      </PageBackground>
+      <Header isHomePage={isHomePage} />
+      <div className={isHomePage ? styles["home-page-section"] : styles.section}>
+        <Routes>
+          {/*TODO: Nest properly according to new react router standard */}
+          <Route path={PATH.HOME} element={<HomePage />} />
+          <Route path={PATH.LETTER} element={<LetterExposition />}></Route>
+          <Route path={PATH.ALPHABET_WITH_LANGUAGE} element={<Alphabet />}></Route>
+          <Route
+            path={PATH.ALPHABET}
+            element={<Navigate to={generatePath(PATH.ALPHABET_WITH_LANGUAGE, { language: LanguageType.LT })} />}
+          />
+          <Route path={PATH.GENOME_FONT} element={<GenomeFont />}></Route>
+          <Route path={PATH.ABOUT} element={<About />}></Route>
+          <Route element={<Navigate to={PATH.HOME} />} />
+        </Routes>
+      </div>
     </PageTransition>
   );
 };
